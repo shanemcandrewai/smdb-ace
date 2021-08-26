b1ed8746dba7fe305ca3c36c1be2e93947e33fad
shane <s_mcandrew@yahoo.com>
2021-08-26 18:45:31 +0200

    add src-min-noconflict

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
- app.use("/ace-builds", express.static("../ace-builds/src-min-noconflict"));
- app.use(express.static("public"));
- app.use("/scripts", express.static("scripts"));
+//app.use("/ace-builds", express.static("scripts/src-min-noconflict"));
+app.use(express.static("public"));
+app.use("/scripts", express.static("scripts"));
 
 // app.get("/", (req, res) => {
 //  res.sendFile("index.html");
 //  res.sendFile("public/require.js");
 // });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
+"use strict";
 require.config({
-  baseUrl: "ace-builds",
+  baseUrl: "scripts/src-min-noconflict",
   // paths: {
   // foo: 'libs/foo-1.1.3'
   // }
 });
-requirejs(["ace"], function() {
-    var editor = ace.edit("editor");
-    editor.session.setMode("ace/mode/json");
+requirejs(["ace"], function () {
+  fetch("test.json")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("editor").innerHTML = data;
+      const editor = ace.edit("editor");
+      editor.session.setMode("ace/mode/json");
+    });
 });
