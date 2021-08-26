778fcdfcb6fea2c8f3eff4ea74492a5541e7eee9
shane <s_mcandrew@yahoo.com>
2021-08-26 19:03:05 +0200

    move to docs

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//app.use("/ace-builds", express.static("scripts/src-min-noconflict"));
-app.use(express.static("public"));
-app.use("/scripts", express.static("scripts"));
-
-// app.get("/", (req, res) => {
-//  res.sendFile("index.html");
-//  res.sendFile("public/require.js");
-// });
+app.use(express.static("docs"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
