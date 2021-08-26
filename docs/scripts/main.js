bd4b8907fd596e8a264176d9888d62d936522e25
shane <s_mcandrew@yahoo.com>
2021-08-27 00:47:29 +0200

    src-min-noconflict for master

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,7 +1,7 @@
 "use strict";
-//requirejs(["ace-builds/src-min-noconflict/ace"], function() {  //works
+requirejs(["ace-builds/src-min-noconflict/ace"], function() {  //works
 //requirejs(["ace-builds/src-min/ace"], function() {  //works sometimes, otherwise Error : Uncaught (in promise) ReferenceError: ace is not defined
-requirejs(["ace/lib/ace/ace"], function() {  //Error : Uncaught (in promise) ReferenceError: ace is not defined
+//requirejs(["ace/lib/ace/ace"], function() {  //Error : Uncaught (in promise) ReferenceError: ace is not defined
   fetch("test.json")
     .then((response) => response.text())
     .then((data) => {
