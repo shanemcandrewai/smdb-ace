dfb5ddbd0c08fec943b95e58ae3107780554d546
shane <s_mcandrew@yahoo.com>
2021-08-28 06:05:57 +0200

    add webpack

---
+++ docs/webpack.config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+    mode: "development",
+    entry: "../src/demo.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    node: {
+        global: false,
+        __filename: "mock",
+        __dirname: "mock",
+    },
+    resolveLoader: {
+        modules: [
+            "node_modules", 
+            __dirname + "../node_modules",
+        ],
+    },
+    devServer: {
+        contentBase: __dirname,
+        compress: true,
+        port: 9000
+    }
+};
