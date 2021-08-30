3a8e39343b7a0d220d0b489a79d1ebe1b7c15f63
shane <s_mcandrew@yahoo.com>
2021-08-30 08:09:30 +0200

    remove ace build libs

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
 "use strict";
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/demo.js",
-	devtool: "source-map",
-    output: {
-        path: __dirname + "/docs/dist",
-        filename: "bundle.js"
-    },
-    node: {
-        global: false,
-        __filename: "mock",
-        __dirname: "mock",
-    },
-    resolveLoader: {
-        modules: [
-            "node_modules", 
-            __dirname + "node_modules",
-        ],
-    },
-    devServer: {
-        contentBase: __dirname,
-        compress: true,
-        port: 9000
-    }
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "docs/scripts/dist"),
+  },
+  node: {
+    global: false,
+    __filename: "mock",
+    __dirname: "mock",
+  },
+  resolveLoader: {
+    modules: ["node_modules", __dirname + "node_modules"],
+  },
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    port: 9000,
+  },
 };
