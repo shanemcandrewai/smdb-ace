012143573183da17fbcdab10d2a5fe1fd60c51f5
shane <s_mcandrew@yahoo.com>
2021-09-02 09:00:04 +0200

    simply webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -8,20 +8,4 @@ module.exports = {
         path: path.resolve(__dirname, "docs/dist"),
         filename: "bundle.js",
   },
-    node: {
-        global: false,
-        __filename: "mock",
-        __dirname: "mock",
-    },
-    resolveLoader: {
-        modules: [
-            "node_modules", 
-            __dirname + "./node_modules",
-        ],
-    },
-    devServer: {
-        contentBase: __dirname,
-        compress: true,
-        port: 9000
-    }
 };
