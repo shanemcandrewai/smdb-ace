526a06123205d485ce5c7ec5bafc8235c85a17da
shane <s_mcandrew@yahoo.com>
2021-09-04 09:53:54 +0200

    init files

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("docs"));
-//app.use("/ace", express.static("docs/scripts/ace-builds/src-min-noconflict")); //works
-//app.use("/ace", express.static("../ace-builds/src-min-noconflict")); //works
-//app.use("/ace", express.static("../ace/lib/ace")); //Error: Loading failed for the <script> with source “http://localhost:3000/ace/ace/requirejs/text.js”.
-//app.use("/ace", express.static("../ace-builds/src-min"));  //works sometimes, other times fails with Error : Uncaught (in promise) ReferenceError: ace is not defined
+app.use("ace", express.static("src/ace"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
