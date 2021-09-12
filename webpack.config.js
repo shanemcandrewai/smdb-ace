f9574841e85d280cc633ccda1db126fa3fc55dfb
shane <s_mcandrew@yahoo.com>
2021-09-12 11:47:59 +0200

    add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "docs/dist"),
+    path: path.resolve(__dirname, "docs"),
+    clean: true,
   },
-  devtool: "inline-source-map",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, "docs"),
+      },
+    ],
+  },
 };
