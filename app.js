4f88a48ad6ec9737038d9606c2e489590f939ee1
shane <s_mcandrew@yahoo.com>
2021-09-29 18:46:18 +0200

    match arrays

--- app.js
+++ app.js
@@ -10,24 +10,35 @@ async function* walk(dir) {
   }
 }
 async function main() {
-  const regex1 = /^\s*define/i;
+  const regex_define = /^\s*define/i;
+  const regex_define_end = /^\s*}\)/;
   const root_dir = "docs/scripts/ace/test/";
   for await (const file_path of walk(root_dir)) {
     fs.promises
       .readFile(file_path)
       .then((result) => {
+        let define_deleted = false;
         const lines = result.toString().split("\n");
         lines.forEach((line, line_no, arr) => {
-          if (line.match(regex1)) {
+          if (line.match(regex_define)) {
             console.log(file_path, line_no, lines[line_no]);
             arr.splice(line_no, 1);
+            define_deleted = true;
           }
         });
+        if (define_deleted) {
+          lines.forEach((line, line_no, arr) => {
+            if (line.match(regex_define_end)) {
+              console.log(file_path, line_no, lines[line_no]);
+              arr.splice(line_no, 1);
+            }
+          });
+        }
         const out_text = lines.join("\n");
         fs.promises
           .writeFile(file_path, out_text)
-          .then((result) => {
-            console.log("file written:", file_path, result);
+          .then(() => {
+            console.log("file written:", file_path);
           })
           .catch((error) => {
             console.log(error);
