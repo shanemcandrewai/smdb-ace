d3448bb3bf19a3840316782bb00c375d51785f99
shane <s_mcandrew@yahoo.com>
2021-09-29 15:37:50 +0200

    implement walk

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-"use strict";
 import { argv } from "process";
-import fs from "fs";
+import * as fs from "fs";
 import path from "path";
 
 async function* walk(dir) {
@@ -10,9 +9,36 @@ async function* walk(dir) {
     else if (d.isFile()) yield entry;
   }
 }
+async function main() {
+  const regex1 = /^\s*define/i;
+  const root_dir = "docs/scripts/ace/test/";
+  for await (const file_path of walk(root_dir)) {
+    fs.promises
+      .readFile(file_path)
+      .then((result) => {
+        const lines = result.toString().split("\n");
+        lines.forEach((line, line_no, arr) => {
+          if (line.match(regex1)) {
+            console.log(file_path, line_no, lines[line_no]);
+            arr.splice(line_no, 1);
+          }
+        });
+        const out_text = lines.join("\n");
+        fs.promises
+          .writeFile(file_path, out_text)
+          .then((result) => {
+            console.log("file written:", file_path, result);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
 
 if (argv[2] == "convert") {
-  (async function () {
-    for await (const p of walk("docs/scripts/ace/test")) console.log(p);
-  })();
+  main();
 }
