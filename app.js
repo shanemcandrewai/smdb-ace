a0e2e398b1566d649fcb5c198e64e517bec1a4ff
shane <s_mcandrew@yahoo.com>
2021-09-30 11:23:53 +0200

    regex trial

--- app.js
+++ app.js
@@ -11,36 +11,24 @@ async function* walk(dir) {
 }
 async function main() {
   const regex_define = /\n*\s*define.*\n/i;
-  const regex_define_end = /\n\s*}\)\s*\n/g;
+  const regex_define_end = /\n*\s*}\).$/;
   const root_dir = "docs/scripts/ace/test/";
   for await (const file_path of walk(root_dir)) {
     fs.promises
       .readFile(file_path)
       .then((results) => {
         let file_contents = results.toString();
-        let define_deleted = false;
-        // Delete define
-        const ret_define = file_contents.match(regex_define);
-        if (ret_define) {
-          console.log("regex_define", file_path, ret_define[0]);
-          define_deleted = true;
+        if (regex_define.test(file_contents)) {
+          // Delete define
+          file_contents = file_contents.replace(regex_define, "");
+          console.log("regex_define found", file_path);
+          // Delete last });
+          file_contents = file_contents.replace(regex_define_end, "");
+          console.log("regex_define_end found", file_path);
         } else {
           console.log("regex_define not found", file_path);
         }
-        // });
-        if (define_deleted) {
-          const ret_define_end = file_contents.matchAll(regex_define_end);
-          for (const match of ret_define_end) {
-            console.log(
-              `Found ${match[0]} start=${match.index} end=${
-                match.index + match[0].length
-              }.`
-            );
-          }
-          if (ret_define) {
-            console.log("regex_define_end", file_path, ret_define[0]);
-          }
-        }
+
         fs.promises
           .writeFile(file_path, file_contents)
           .then(() => {
