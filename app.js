a94dd39e9bd3319ba306e05cb77d43c151a440c8
shane <s_mcandrew@yahoo.com>
2021-10-02 21:24:49 +0200

    function replace_es6

--- app.js
+++ app.js
@@ -9,74 +9,80 @@ async function* walk(dir) {
     else if (d.isFile()) yield entry;
   }
 }
+
+function replace_es6(file_in, regex, import_statement) {
+  for (const found of [...file_in.contents.matchAll(regex)]) {
+    console.log("found", found[0]);
+    console.log("import", eval("`" + import_statement + "`"));
+    file_in.contents = file_in.contents.replace(
+      found[0],
+      eval("`" + import_statement + "`")
+    );
+    file_in.changed = true;
+  }
+  return file_in;
+}
+
 async function main() {
-  const regex_define = /(^|\s)define.*\n/;
-  const regex_define_end = /}\);?\s*(?=if\s\(ty|$)/;
-  const regex_require = /\nrequire\("(?<req_name>.*)"\)"*.\n/g;
-  const regex_var =
-    /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)*.\n/g;
-  const regex_var_member =
-    /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+);\s*\n/g;
-  const root_dir = "docs/scripts/ace/";
-  const regex_mod_exports = /\nmodule\.exports\s*=\s*(?<var_name>\w+);\s*\n/g;
-  const regex_mod_exports_br = /\nmodule\.exports\s*=\s*{\n/g;
-  const regex_exports_var = /\nexports\.(?<var_name>\w+)\s*/g;
+  const root_dir = "docs/scripts/ace/test";
+
   for await (const file_path of walk(root_dir)) {
     fs.promises
       .readFile(file_path)
       .then((results) => {
-        let file_changed = false;
-        let file_contents = results.toString();
-        if (regex_define.test(file_contents)) {
-          // Delete define
-          file_contents = file_contents.replace(regex_define, "");
-          file_changed = true;
-          console.log("regex_define found", file_path);
-          // Delete last }) or });
-          file_contents = file_contents.replace(regex_define_end, "");
-          console.log("regex_define_end executed", file_path);
-        } else {
-          console.log("regex_define not found", file_path);
+        console.log("xxx file_path", file_path);
+        let file_in = { changed: false, contents: results.toString() };
+        // delete define
+        const regex_define = /(^|\n)define\(.*\n/g;
+        file_in = replace_es6(file_in, regex_define, "");
+        if (file_in.changed) {
+          // delete matching }/;
+          const regex_define_end = /}\);?\s*(?=if\s\(ty|$)/;
+          file_in.contents = file_in.contents.replace(
+            file_in,
+            regex_define_end,
+            ""
+          );
         }
         // require("req_name")
-        for (const found of [...file_contents.matchAll(regex_require)]) {
-          const import_statement = `\nimport * from "${found.groups.req_name}.js";\n`;
-          file_contents = file_contents.replace(found[0], import_statement);
-          file_changed = true;
-        }
+        const regex_require = /\nrequire\("(?<req_name>.*)"\)"*.\n/g;
+        let import_statement =
+          '\nimport * from "${found.groups.req_name}.js";\n';
+        file_in = replace_es6(file_in, regex_require, import_statement);
+
         // var var_name = require("module_name")
-        for (const found of [...file_contents.matchAll(regex_var)]) {
-          const import_statement = `\nimport * as ${found.groups.var_name} from "${found.groups.module_name}.js";\n`;
-          file_contents = file_contents.replace(found[0], import_statement);
-          file_changed = true;
-        }
+        const regex_var =
+          /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)*.\n/g;
+        import_statement =
+          '\nimport * as ${found.groups.var_name} from "${found.groups.module_name}.js";\n';
+        file_in = replace_es6(file_in, regex_var, import_statement);
+
         // var var_name = require("module_name').member
-        for (const found of [...file_contents.matchAll(regex_var_member)]) {
-          const import_statement = `\nimport { ${found.groups.member_name} } as ${found.groups.var_name} from "${found.groups.module_name}.js";\n`;
-          file_contents = file_contents.replace(found[0], import_statement);
-          file_changed = true;
-        }
+        const regex_var_member =
+          /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+);\s*\n/g;
+        import_statement =
+          '\nimport { ${found.groups.member_name} } as ${found.groups.var_name} from "${found.groups.module_name}.js";\n';
+        file_in = replace_es6(file_in, regex_var_member, import_statement);
+
         // module.exports = var_name
-        for (const found of [...file_contents.matchAll(regex_mod_exports)]) {
-          const import_statement = `\nexport { ${found.groups.var_name} };\n`;
-          file_contents = file_contents.replace(found[0], import_statement);
-          file_changed = true;
-        }
+        const regex_mod_exports =
+          /\nmodule\.exports\s*=\s*(?<var_name>\w+);\s*\n/g;
+        import_statement = "\nexport { ${found.groups.var_name} };\n";
+        file_in = replace_es6(file_in, regex_mod_exports, import_statement);
+
         // module.exports = {
-        for (const found of [...file_contents.matchAll(regex_mod_exports_br)]) {
-          const import_statement = "\nexport {";
-          file_contents = file_contents.replace(found[0], import_statement);
-          file_changed = true;
-        }
+        const regex_mod_exports_br = /\nmodule\.exports\s*=\s*{\n/g;
+        import_statement = "\nexport {";
+        file_in = replace_es6(file_in, regex_mod_exports_br, import_statement);
+
         //exports.var_nam =
-        for (const found of [...file_contents.matchAll(regex_exports_var)]) {
-          const import_statement = `\nexport ${found.groups.var_name} `;
-          file_contents = file_contents.replace(found[0], import_statement);
-          file_changed = true;
-        }
-        if (file_changed) {
+        const regex_exports_var = /\nexports\.(?<var_name>\w+)\s*/g;
+        import_statement = "\nexport ${found.groups.var_name} ";
+        file_in = replace_es6(file_in, regex_exports_var, import_statement);
+
+        if (file_in.changed) {
           fs.promises
-            .writeFile(file_path, file_contents)
+            .writeFile(file_path, file_in.contents)
             .then(() => {
               console.log("file written:", file_path);
             })
