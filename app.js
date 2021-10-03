9877cf1202f6dd60af38975c906f4dce7b14f0ed
shane <s_mcandrew@yahoo.com>
2021-10-03 11:51:00 +0200

    fix rules

--- app.js
+++ app.js
@@ -12,17 +12,21 @@ function Rule(regex, subst_templ, prev_changed, description) {
 function get_rules() {
   return [
     new Rule(new RegExp(/(^|\n)define\(.*\n/, "g"), "", "", "delete define"),
+    new Rule(new RegExp(/\n}\);?\s*$/, "g"), "", true, "delete matching });"),
     new Rule(
-      new RegExp(/}\);?\s*(?=if\s\(ty|$)/, "g"),
-      "",
-      true,
-      "delete matching });"
-    ),
-    new Rule(
-      new RegExp(/\nrequire\("(?<req_name>.*)"\);/, "g"),
-      '\nimport * from "${found.groups.req_name}.js";',
+      new RegExp(/\nrequire\("(?<req_path>.*\/)(?<req_name>.*)"\)/, "g"),
+      '\nimport * as ${found.groups.req_name} from "${found.groups.req_path}${found.groups.req_name}.js"',
       "",
       "require('req_name')"
+    ),
+    new Rule(
+      new RegExp(
+        /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+)/,
+        "g"
+      ),
+      '\nimport { ${found.groups.member_name} as ${found.groups.var_name} } from "${found.groups.module_name}.js"',
+      "",
+      "var var_name = require('module_name').member"
     ),
     new Rule(
       new RegExp(
@@ -33,25 +37,18 @@ function get_rules() {
       "",
       "var var_name = require('module_name')"
     ),
-
     new Rule(
       new RegExp(
-        /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+);\s*\n/,
+        /\nexports\.(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\);/,
         "g"
       ),
-      '\nimport { ${found.groups.member_name} } as ${found.groups.var_name} from "${found.groups.module_name}.js";\n',
-      "",
-      "var var_name = require('module_name').member"
-    ),
-    new Rule(
-      new RegExp(/\nexports\.(?<var_name>\w+)\s*/, "g"),
-      '\nexport ${found.groups.var_name} from "${found.groups.module_name}.js"',
+      '\nexport { ${found.groups.var_name} } from "${found.groups.module_name}.js"',
       "",
       "exports.var_nam = require('module_name')"
     ),
     new Rule(
-      new RegExp(/\nmodule\.exports\.(?<var_name>\w+)\s*/, "g"),
-      "\nexport { ${found.groups.var_name} };\n",
+      new RegExp(/\nmodule\.exports\.(?<var_name>\w+)/, "g"),
+      "\nexport { ${found.groups.var_name} }",
       "",
       "module.exports = var_name"
     ),
@@ -62,13 +59,10 @@ function get_rules() {
       "module.exports = {"
     ),
     new Rule(
-      new RegExp(
-        /\nexports\.(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\);/,
-        "g"
-      ),
-      "\nexport ${found.groups.var_name} ",
+      new RegExp(/\nexports\.(?<var_name>\w+)/, "g"),
+      "\nexport let ${found.groups.var_name}",
       "",
-      "exports.var_nam = require("
+      "exports = var_name"
     ),
   ];
 }
@@ -83,7 +77,8 @@ async function* walk(dir) {
 
 function process(file_in, rule) {
   for (const found of [...file_in.contents.matchAll(rule.regex)]) {
-    console.log("\nrule      :", rule.description);
+    console.log("\nchange    :", file_in.change);
+    console.log("rule      :", rule.description);
     console.log("found     :", JSON.stringify(found[0]));
     console.log(
       "replaced  :",
@@ -93,8 +88,10 @@ function process(file_in, rule) {
       found[0],
       eval("`" + rule.subst_templ + "`")
     );
+    file_in.change++;
     file_in.changed = true;
   }
+
   return file_in;
 }
 
@@ -106,7 +103,11 @@ async function main() {
       .readFile(file_path)
       .then((results) => {
         console.log("\nfile path :", file_path);
-        let file_in = { changed: false, contents: results.toString() };
+        let file_in = {
+          changed: false,
+          contents: results.toString(),
+          change: 0,
+        };
 
         for (const rule of get_rules()) {
           if (rule.prev_change == null || rule.prev_change == file_in.changed) {
