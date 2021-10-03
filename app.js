b0c68142bc87134b28d31edc52629ff0972de511
shane <s_mcandrew@yahoo.com>
2021-10-03 10:09:23 +0200

    introduce rules

--- app.js
+++ app.js
@@ -2,6 +2,77 @@ import { argv } from "process";
 import * as fs from "fs";
 import path from "path";
 
+function Rule(regex, subst_templ, prev_changed, description) {
+  this.regex = regex;
+  this.subst_templ = subst_templ;
+  this.prev_changed = prev_changed;
+  this.description = description;
+}
+
+function get_rules() {
+  return [
+    new Rule(new RegExp(/(^|\n)define\(.*\n/, "g"), "", "", "delete define"),
+    new Rule(
+      new RegExp(/}\);?\s*(?=if\s\(ty|$)/, "g"),
+      "",
+      true,
+      "delete matching });"
+    ),
+    new Rule(
+      new RegExp(/\nrequire\("(?<req_name>.*)"\);/, "g"),
+      '\nimport * from "${found.groups.req_name}.js";',
+      "",
+      "require('req_name')"
+    ),
+    new Rule(
+      new RegExp(
+        /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)/,
+        "g"
+      ),
+      '\nimport * as ${found.groups.var_name} from "${found.groups.module_name}.js"',
+      "",
+      "var var_name = require('module_name')"
+    ),
+
+    new Rule(
+      new RegExp(
+        /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+);\s*\n/,
+        "g"
+      ),
+      '\nimport { ${found.groups.member_name} } as ${found.groups.var_name} from "${found.groups.module_name}.js";\n',
+      "",
+      "var var_name = require('module_name').member"
+    ),
+    new Rule(
+      new RegExp(/\nexports\.(?<var_name>\w+)\s*/, "g"),
+      '\nexport ${found.groups.var_name} from "${found.groups.module_name}.js"',
+      "",
+      "exports.var_nam = require('module_name')"
+    ),
+    new Rule(
+      new RegExp(/\nmodule\.exports\.(?<var_name>\w+)\s*/, "g"),
+      "\nexport { ${found.groups.var_name} };\n",
+      "",
+      "module.exports = var_name"
+    ),
+    new Rule(
+      new RegExp(/\nmodule\.exports\s*=\s*{\n/, "g"),
+      "\nexport {",
+      "",
+      "module.exports = {"
+    ),
+    new Rule(
+      new RegExp(
+        /\nexports\.(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\);/,
+        "g"
+      ),
+      "\nexport ${found.groups.var_name} ",
+      "",
+      "exports.var_nam = require("
+    ),
+  ];
+}
+
 async function* walk(dir) {
   for await (const d of await fs.promises.opendir(dir)) {
     const entry = path.join(dir, d.name);
@@ -10,13 +81,17 @@ async function* walk(dir) {
   }
 }
 
-function replace_es6(file_in, regex, import_statement) {
-  for (const found of [...file_in.contents.matchAll(regex)]) {
-    console.log("found", found[0]);
-    console.log("import", eval("`" + import_statement + "`"));
+function process(file_in, rule) {
+  for (const found of [...file_in.contents.matchAll(rule.regex)]) {
+    console.log("\nrule      :", rule.description);
+    console.log("found     :", JSON.stringify(found[0]));
+    console.log(
+      "replaced  :",
+      JSON.stringify(eval("`" + rule.subst_templ + "`"))
+    );
     file_in.contents = file_in.contents.replace(
       found[0],
-      eval("`" + import_statement + "`")
+      eval("`" + rule.subst_templ + "`")
     );
     file_in.changed = true;
   }
@@ -24,61 +99,20 @@ function replace_es6(file_in, regex, import_statement) {
 }
 
 async function main() {
-  const root_dir = "docs/scripts/ace/test";
+  const root_dir = "docs/scripts/ace/";
 
   for await (const file_path of walk(root_dir)) {
     fs.promises
       .readFile(file_path)
       .then((results) => {
-        console.log("xxx file_path", file_path);
+        console.log("\nfile path :", file_path);
         let file_in = { changed: false, contents: results.toString() };
-        // delete define
-        const regex_define = /(^|\n)define\(.*\n/g;
-        file_in = replace_es6(file_in, regex_define, "");
-        if (file_in.changed) {
-          // delete matching }/;
-          const regex_define_end = /}\);?\s*(?=if\s\(ty|$)/;
-          file_in.contents = file_in.contents.replace(
-            file_in,
-            regex_define_end,
-            ""
-          );
+
+        for (const rule of get_rules()) {
+          if (rule.prev_change == null || rule.prev_change == file_in.changed) {
+            file_in = process(file_in, rule);
+          }
         }
-        // require("req_name")
-        const regex_require = /\nrequire\("(?<req_name>.*)"\)"*.\n/g;
-        let import_statement =
-          '\nimport * from "${found.groups.req_name}.js";\n';
-        file_in = replace_es6(file_in, regex_require, import_statement);
-
-        // var var_name = require("module_name")
-        const regex_var =
-          /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)*.\n/g;
-        import_statement =
-          '\nimport * as ${found.groups.var_name} from "${found.groups.module_name}.js";\n';
-        file_in = replace_es6(file_in, regex_var, import_statement);
-
-        // var var_name = require("module_name').member
-        const regex_var_member =
-          /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+);\s*\n/g;
-        import_statement =
-          '\nimport { ${found.groups.member_name} } as ${found.groups.var_name} from "${found.groups.module_name}.js";\n';
-        file_in = replace_es6(file_in, regex_var_member, import_statement);
-
-        // module.exports = var_name
-        const regex_mod_exports =
-          /\nmodule\.exports\s*=\s*(?<var_name>\w+);\s*\n/g;
-        import_statement = "\nexport { ${found.groups.var_name} };\n";
-        file_in = replace_es6(file_in, regex_mod_exports, import_statement);
-
-        // module.exports = {
-        const regex_mod_exports_br = /\nmodule\.exports\s*=\s*{\n/g;
-        import_statement = "\nexport {";
-        file_in = replace_es6(file_in, regex_mod_exports_br, import_statement);
-
-        //exports.var_nam =
-        const regex_exports_var = /\nexports\.(?<var_name>\w+)\s*/g;
-        import_statement = "\nexport ${found.groups.var_name} ";
-        file_in = replace_es6(file_in, regex_exports_var, import_statement);
 
         if (file_in.changed) {
           fs.promises
