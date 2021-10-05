cb0fb8fe2d0a1f95bc33342edf993c3fd29a8963
shane <s_mcandrew@yahoo.com>
2021-10-05 13:33:17 +0200

    const rules

--- cjs_to_es6.js
+++ cjs_to_es6.js
@@ -8,60 +8,58 @@ function Rule(regex, subst_templ, precondition, description) {
   this.description = description;
 }
 
-function get_rules() {
-  return [
-    // lookbehind assertion with variable width works but is too slow
-    // new Rule(
-    // new RegExp(/(?<=(^|\n)define\(.*\n([\s\S]*))\n}\);?\s*$/, "g"),
-    // "",
-    // "delete matching }) for define;"
-    // ),
-    new Rule(/(^|\n)define\(.*\n/g, null, "", "delete define"),
-    new Rule(/\n}\);?\s*$/g, "", true, "delete matching });"),
-    new Rule(
-      /\nrequire\("(?<req_path>.*\/)(?<req_name>.*)"\)/g,
-      '\nimport * as $<req_name> from "$<req_path>$<req_name>.js"',
-      "",
-      "require('req_name')"
-    ),
-    new Rule(
-      /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+)/g,
-      '\nimport { $<member_name> as $<var_name> } from "$<module_name>.js"',
-      null,
-      "var var_name = require('module_name').member"
-    ),
-    new Rule(
-      /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)/,
-      '\nimport * as $<var_name> from "$<module_name>.js"',
-      null,
-      "var var_name = require('module_name')"
-    ),
-    new Rule(
-      /\nexports\.(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\);/,
-      '\nexport { $<var_name> } from "$<module_name>.js"',
-      null,
-      "exports.var_nam = require('module_name')"
-    ),
-    new Rule(
-      /\nmodule\.exports\.(?<var_name>\w+)/g,
-      "\nexport { $<var_name> }",
-      null,
-      "module.exports = var_name"
-    ),
-    new Rule(
-      /\nmodule\.exports\s*=\s*{/g,
-      "\nexport {",
-      null,
-      "module.exports = {"
-    ),
-    new Rule(
-      /\nexports\.(?<var_name>\w+)\s=\sfunction/g,
-      "\nexport let $<var_name> = function",
-      null,
-      "exports = var_name"
-    ),
-  ];
-}
+const rules = [
+  // lookbehind assertion with variable width works but is too slow
+  // new Rule(
+  // new RegExp(/(?<=(^|\n)define\(.*\n([\s\S]*))\n}\);?\s*$/, "g"),
+  // "",
+  // "delete matching }) for define;"
+  // ),
+  new Rule(/(^|\n)define\(.*\n/g, "", null, "delete define"),
+  new Rule(/\n}\);?\s*$/g, "", true, "delete matching });"),
+  new Rule(
+    /\nrequire\("(?<req_path>.*\/)(?<req_name>.*)"\)/g,
+    '\nimport * as $<req_name> from "$<req_path>$<req_name>.js"',
+    "",
+    "require('req_name')"
+  ),
+  new Rule(
+    /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)\.(?<member_name>\w+)/g,
+    '\nimport { $<member_name> as $<var_name> } from "$<module_name>.js"',
+    null,
+    "var var_name = require('module_name').member"
+  ),
+  new Rule(
+    /\nvar\s+(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\)/,
+    '\nimport * as $<var_name> from "$<module_name>.js"',
+    null,
+    "var var_name = require('module_name')"
+  ),
+  new Rule(
+    /\nexports\.(?<var_name>\w+)\s*=\s*require\("(?<module_name>.*)"\);/,
+    '\nexport { $<var_name> } from "$<module_name>.js"',
+    null,
+    "exports.var_nam = require('module_name')"
+  ),
+  new Rule(
+    /\nmodule\.exports\.(?<var_name>\w+)/g,
+    "\nexport { $<var_name> }",
+    null,
+    "module.exports = var_name"
+  ),
+  new Rule(
+    /\nmodule\.exports\s*=\s*{/g,
+    "\nexport {",
+    null,
+    "module.exports = {"
+  ),
+  new Rule(
+    /\nexports\.(?<var_name>\w+)\s=\sfunction/g,
+    "\nexport let $<var_name> = function",
+    null,
+    "exports = var_name"
+  ),
+];
 
 async function* walk(dir) {
   for await (const d of await fs.promises.opendir(dir)) {
@@ -101,7 +99,7 @@ async function main() {
           change: 0,
         };
 
-        for (const rule of get_rules()) {
+        for (const rule of rules) {
           if (
             rule.precondition == null ||
             rule.precondition == file_in.changed
