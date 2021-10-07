c3bd62c36f90e77ef2e1e9144655df0763981f53
shane <s_mcandrew@yahoo.com>
2021-10-07 13:27:29 +0200

    add require statements for node REPL

--- recast_ace.js
+++ recast_ace.js
@@ -1,5 +1,7 @@
 import { parse, print } from "recast";
+// const { parse, print } = require("recast");
 import { readFileSync } from "fs";
+// const { readFileSync } = require("fs");
 
 const acefile = "docs/ace-builds/src-noconflict/ace.js";
 let ast = parse(readFileSync(acefile).toString());
