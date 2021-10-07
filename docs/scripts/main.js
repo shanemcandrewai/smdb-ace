25bcf3ec703be6e86046cdf7aceec950dab17b7a
shane <s_mcandrew@yahoo.com>
2021-10-07 13:57:14 +0200

    update ace lib v1.4.13

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,12 +1,10 @@
-"use strict";
-import * as ace from './ace/ace.js'
-  const editor = ace.edit(null, {
-    maxLines: 50,
-    minLines: 10,
-    value:
-      '{\n  "array": [1, 2, 3],\n  "boolean":, true,\n  "color": "gold",\n  "null": null,\n  "number": 123,\n  "object": {\n    "a": "b",\n    "c": "d"\n  },\n  "string": "Hello World"\n}\n',
-    mode: "ace/mode/json",
-  });
+import * as ace from "./ace/ace.js";
+const editor = ace.edit(null, {
+  maxLines: 50,
+  minLines: 10,
+  value:
+    '{\n  "array": [1, 2, 3],\n  "boolean":, true,\n  "color": "gold",\n  "null": null,\n  "number": 123,\n  "object": {\n    "a": "b",\n    "c": "d"\n  },\n  "string": "Hello World"\n}\n',
+  mode: "ace/mode/json",
+});
 
-  document.body.appendChild(editor.container);
-
+document.body.appendChild(editor.container);
