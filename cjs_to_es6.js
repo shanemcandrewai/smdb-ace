560562fc9083038cb70038112b7b968d97a54439
shane <s_mcandrew@yahoo.com>
2021-10-10 16:25:16 +0200

    simply winston

--- cjs_to_es6.js
+++ cjs_to_es6.js
@@ -8,32 +8,16 @@ const { createLogger, format, transports } = winston;
 const rootDir = 'docs/scripts/ace/test';
 
 const logger = createLogger({
-  level: 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
     }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json(),
-  ),
-  transports: [new transports.File({ filename: 'output.log' })],
+  ],
 });
 
-function initLogger() {
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-      new transports.Console({
-        format: format.combine(format.colorize(), format.simple()),
-      }),
-    );
-  }
-}
-
 async function* walk(dir) {
-  logger.info(`walk ${dir}`);
   for await (const dirFile of await opendir(dir)) { // eslint-disable-line no-restricted-syntax
-    logger.info(`walk inner ${dirFile}`);
+    logger.info(`walk inner ${dirFile.name}`);
     const entry = join(dir, dirFile.name);
     if (dirFile.isDirectory()) yield* walk(entry);
     else if (dirFile.isFile()) yield (entry);
@@ -41,12 +25,11 @@ async function* walk(dir) {
 }
 
 function processContents(fileIn) {
-  logger.log('process file contests', fileIn.filepath);
+  logger.info('process file contents');
+  return fileIn;
 }
 
 async function processFile(filepath) {
-  logger.log('file path :', filepath);
-
   const results = await readFile(filepath);
 
   let fileIn = {
@@ -64,7 +47,6 @@ async function processFile(filepath) {
 }
 
 async function main() {
-  initLogger();
   logger.info('start logger');
 
   if (argv.length === 3) {
@@ -76,7 +58,7 @@ async function main() {
   } else {
     logger.info('argv.length !== 3');
     for await (const filePath of walk(rootDir)) { // eslint-disable-line no-restricted-syntax
-      logger.info(filePath);
+      processFile(filePath);
     }
   }
 }
