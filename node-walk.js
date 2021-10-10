2438e9fb7fcf3e767908076dc7c0c5e33023e46d
shane <s_mcandrew@yahoo.com>
2021-10-10 15:16:37 +0200

    improve node-walk

--- node-walk.js
+++ node-walk.js
@@ -2,9 +2,7 @@ import { opendir } from 'fs/promises';
 import { join } from 'path';
 import winston from 'winston';
 
-const createLogger = winston.createLogger; // eslint-disable-line prefer-destructuring
-const format = winston.format; // eslint-disable-line prefer-destructuring
-const transports = winston.transports; // eslint-disable-line prefer-destructuring
+const { createLogger, format, transports } = winston;
 
 const logger = createLogger({
   level: 'info',
@@ -33,7 +31,7 @@ async function* walk(dir) {
   for await (const d of await opendir(dir)) { // eslint-disable-line no-restricted-syntax
     const entry = join(dir, d.name);
     if (d.isDirectory()) yield* walk(entry);
-    else if (d.isFile()) yield entry;
+    else if (d.isFile()) yield (entry);
   }
 }
 
