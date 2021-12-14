d28e96694b9eb82fb2b6dbd3870ca5fda3f5ca72
shane <s_mcandrew@yahoo.com>
2021-12-14 18:42:46 +0100

    textarea

--- node-walk.js
+++ node-walk.js
@@ -1,44 +1,46 @@
-import { opendir } from 'fs/promises';
-import { join } from 'path';
-import winston from 'winston';
+import { opendir } from "fs/promises";
+import { join } from "path";
+import winston from "winston";
 
 const { createLogger, format, transports } = winston;
 
 const logger = createLogger({
-  level: 'info',
+  level: "info",
   format: format.combine(
     format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
+      format: "YYYY-MM-DD HH:mm:ss",
     }),
     format.errors({ stack: true }),
     format.splat(),
-    format.json(),
+    format.json()
   ),
-  transports: [new transports.File({ filename: 'output.log' })],
+  transports: [new transports.File({ filename: "output.log" })],
 });
 
 function initLogger() {
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     logger.add(
       new transports.Console({
         format: format.combine(format.colorize(), format.simple()),
-      }),
+      })
     );
   }
 }
 
 async function* walk(dir) {
-  for await (const dirFile of await opendir(dir)) { // eslint-disable-line no-restricted-syntax
+  for await (const dirFile of await opendir(dir)) {
+    // eslint-disable-line no-restricted-syntax
     const entry = join(dir, dirFile.name);
     if (dirFile.isDirectory()) yield* walk(entry);
-    else if (dirFile.isFile()) yield (entry);
+    else if (dirFile.isFile()) yield entry;
   }
 }
 
 async function main() {
   initLogger();
-  logger.info('start logger');
-  for await (const filePath of walk('docs/scripts/ace/test')) { // eslint-disable-line no-restricted-syntax
+  logger.info("start logger");
+  for await (const filePath of walk("docs/scripts/ace/test")) {
+    // eslint-disable-line no-restricted-syntax
     logger.info(filePath);
   }
 }
